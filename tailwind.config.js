1608deabb15dfaadbfa516bfb7d360db1207696e
yuliiahotsii <137622673+fannciful@users.noreply.github.com>
2024-05-20 14:53:14 +0300

    Add files via upload

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'neutral-gr1': '#a3a3a3',
+        'neutral-gr2': '#737373',
+      },
+    },
+  },
+  plugins: [],
+}
